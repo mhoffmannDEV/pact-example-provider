d9d2070166f4524a4e0fdfbb87f76fc6d5541799
mhoffmannDEV <mhoffmann@bluesoft.com>
2024-10-29 15:47:36 +0100

    Update product.js (dodanie alias do modelu)

--- src/product/product.js
+++ src/product/product.js
@@ -1,9 +1,10 @@
 class Product {
-    constructor(id, type, name, version) {
+    constructor(id, type, name, version, alias) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
+        this.alias = alias;
     }
 }
 
